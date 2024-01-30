6c9818d718a81b1c557bff6add28ac4bcc2db332
Nikolas Spyropoulos <nick.mccrafter@gmail.com>
2024-01-31 00:01:03 +0200

    Added velocity and acceleration

--- core/celestial_body.py
+++ core/celestial_body.py
@@ -1,6 +1,16 @@
-from ursina import Entity
+from ursina import Entity, time
 
 class CelestialBody(Entity):
-    def __init__(self, mass=1.0, add_to_scene_entities=True, **kwargs):
+    def __init__(self, name, mass=1.0, velocity=[0, 0, 0], acceleration=[0,0,0], add_to_scene_entities=True, **kwargs):
         super().__init__(add_to_scene_entities, **kwargs)
+        self.name = name
         self.mass = mass
+        self.velocity = velocity # (vx, vy, vz)
+        self.acceleration = acceleration # (ax, ay, az)
+
+    def update_vectors(self):
+        for i in range(3):
+            self.velocity[i] += self.acceleration[i] * time.dt
+        self.x += self.velocity[0] * time.dt
+        self.y += self.velocity[1] * time.dt
+        self.z += self.velocity[2] * time.dt
--- main.py
+++ main.py
@@ -7,18 +7,42 @@ app = Ursina()
 
 camera = FirstPersonController(gravity=0, z=-3, y=-2)
 
-sun = CelestialBody(mass=1.0, model="sphere", color=color.yellow, scale=2, shader=lit_with_shadows_shader)
+sun = CelestialBody("Sun", mass=100.0, model="sphere", color=color.yellow, scale=2, shader=lit_with_shadows_shader)
 light = PointLight(parent=sun, color=color.white)
 light.position = (0, 0, 0)
 
-planet = CelestialBody(mass=0.1, model="sphere", color=color.blue, scale=0.5, shader=lit_with_shadows_shader)
+planet = CelestialBody("Planet 1", mass=0.1, model="sphere", color=color.blue, scale=0.5, shader=lit_with_shadows_shader)
 planet.position = (-2,0,0)
+planet.velocity = [0,0,0] #it works, must set otherwise sun moves too (?!!??!)
+planet.acceleration = [0,0,0]
 
 speed_y = 2
+bodies = [sun, planet]
+
+for body in bodies:
+    print(body.name, body.acceleration)
+
+def calculate_gravity(body1, body2):
+    dx = body2.x - body1.x
+    dy = body2.y - body1.y
+    distance = math.sqrt(dx**2 + dy**2)
+    force = 6.67430e-11 * (body1.mass * body2.mass) / distance**2
+    angle = math.atan2(dy, dx)
+    fx = force * math.cos(angle)
+    fy = force * math.sin(angle)
+    return fx, fy
+
+
 
 def update():
     camera.y += held_keys['space'] * time.dt * speed_y
     camera.y -= held_keys['shift'] * time.dt * speed_y
+
+    for body in bodies:
+        body.update_vectors()
+
+    fx, fz = calculate_gravity(sun, planet)
+    planet.acceleration = (fx, 0, fz)
 
 def input(key):
     #print(key)
